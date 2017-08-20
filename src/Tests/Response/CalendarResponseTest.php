64b962a6ae4314672199195a45540b7f35d45bd2
Franziska Dyckhoff <fdyckhoff@dyvelop.de>
2017-08-20 12:22:40 +0200

    test utf-8 encoding tests #4

--- src/Tests/Response/CalendarResponseTest.php
+++ src/Tests/Response/CalendarResponseTest.php
@@ -39,4 +39,20 @@ class CalendarResponseTest extends CalendarTestCase
         $this->assertContains($calendar->getContentType(), $response->headers->get('Content-Type'));
         $this->assertContains($calendar->getConfig('filename'), $response->headers->get('Content-Disposition'));
     }
+
+
+    /**
+     * Testing UTF-8 encoding
+     */
+    public function testUTF8Encoding()
+    {
+        $calendar = new Calendar(array('format' => 'ical'));
+        $event = $calendar->newEvent();
+        $event->setDtstart('2017/01/01 9:00:00');
+        $event->setDtend('2017/01/01 10:00:00');
+        $event->setSummary('Some event with special chars like ÄÖÜß');
+
+        $response = new CalendarResponse($calendar);
+        $this->assertContains('ÄÖÜß', $response->getContent());
+    }
 }
